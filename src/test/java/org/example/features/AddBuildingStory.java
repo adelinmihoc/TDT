2029e84a4d8b6987a0a24cb726733dd735280fb8
adelin <adelinmihoc@gmail.com>
2022-05-17 20:13:05 +0300

    update

---
+++ src/test/java/org/example/features/AddBuildingStory.java
@@ -0,0 +1,25 @@
+package org.example.features;
+
+import net.thucydides.core.annotations.Steps;
+import org.example.steps.serenity.AddBuildingSteps;
+import org.junit.Test;
+
+public class AddBuildingStory extends BaseStory {
+
+    @Steps
+    private AddBuildingSteps addBuildingSteps;
+
+    @Test
+    public void addBuildingTest() throws InterruptedException {
+        loginSteps.isTheLoginPage();
+        loginSteps.login("adelinmihoc@gmail.com", "X9zfxhZeU6Py9WD");
+        Thread.sleep(2000);
+        addBuildingSteps.isTheAddBuildingPage();
+        Thread.sleep(2000);
+        addBuildingSteps.clickAddBuilding();
+        addBuildingSteps.enterBuildingName("buildingName");
+        addBuildingSteps.enterSafeDistance("150");
+        Thread.sleep(2000);
+        addBuildingSteps.submit();
+    }
+}
